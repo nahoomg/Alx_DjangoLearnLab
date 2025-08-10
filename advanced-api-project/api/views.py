076830635df57f8683368f618a5bc5c5d3efc9d7
nahom <66465737+nahoomg@users.noreply.github.com>
2025-08-10 20:39:07 +0000

    feat: add API URL routing and BookViewSet for CRUD operations

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# advanced_api_project/api/views.py
+from rest_framework import viewsets, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing Book instances.
+    This single class handles all CRUD operations.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
