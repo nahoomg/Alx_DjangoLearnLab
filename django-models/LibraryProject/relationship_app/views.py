a2c8d7f2c1abe90e73e4480e28b7b7c86292b130
nahoomg <nahomgetachewmy@gmail.com>
2025-08-13 03:13:08 +0300

    m

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library  # Ensure this line is present
-
+from .models import Book
+from .models import Library # Add this line
 # Function-based view to list all books
 def book_list(request):
     books = Book.objects.all()
