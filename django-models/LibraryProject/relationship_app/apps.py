e2b2e959708f1fbc678e1fe1039aaaeaafaf4682
nahoomg <nahomgetachewmy@gmail.com>
2025-08-13 18:38:58 +0300

    k

---
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class RelationshipAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LibraryProject.relationship_app'
+    label = 'relationship_app' # Explicitly set the label
