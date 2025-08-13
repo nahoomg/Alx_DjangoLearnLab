fffd7c9c4da8b68b090698bfddfd579aea7e849d
nahoomg <nahomgetachewmy@gmail.com>
2025-08-13 18:27:55 +0300

    oi

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver # Add this line
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -26,3 +29,28 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+    
+class UserProfile(models.Model):
+    # Link to Django's built-in User model with a one-to-one relationship
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Define user roles
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile ({self.role})"
+
+# Django Signal: Automatically create a UserProfile when a new User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
