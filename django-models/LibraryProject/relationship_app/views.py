f9ed9066f0f68660dc4e4928eb5b99fab1130c80
nahoomg <nahomgetachewmy@gmail.com>
2025-08-13 03:18:09 +0300

    ll

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from .models import Book
 from .models import Library
 
 # Function-based view to list all books
-def book_list(request):
+def list_books(request):  # Renamed from book_list to list_books
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'relationship_app/list_books.html', context)
